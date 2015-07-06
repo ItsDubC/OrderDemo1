146246611027260218e4a611b9ff6109c3cea33a
ItsDubC <itsdubc@gmail.com>
2015-07-05 22:00:44 -0500

    Added BLL objects

---
+++ OrderDemo1.BLL/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDemo1.BLL
+{
+    public class Order
+    {
+        public Customer Customer { get; set; }
+        public List<LineItem> LineItems { get; set; }
+        public decimal TotalCost { get; set; }
+
+        public Order() {}
+
+        public Order(Customer customer, List<LineItem> lineItems)
+        {
+            Customer = customer;
+            LineItems = lineItems;
+        }
+    }
+}
